97ca28dc72528e78958f9290d049ff7a92df24d5
Henry Bradlow <hbradlow@berkeley.edu>
2012-08-29 16:27:42 -0700

    Switched to web branch

--- iOS App/Agri Cal/MealViewController.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface MealViewController : UIViewController <UIWebViewDelegate>
-@property (nonatomic, strong) NSDictionary *items;
-@property (nonatomic, strong) UIWebView *web;
-@end
--- iOS App/Agri Cal/SettingsViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "InfoViewController.h"
-
-@interface SettingsViewController : UIViewController <UITextFieldDelegate, UIWebViewDelegate, UIWebViewDelegate>
-@property (weak, nonatomic) IBOutlet UITextField *username;
-@property (weak, nonatomic) IBOutlet UITextField *password;
-@end
